551e77cfdfc2871d8fa1d27839b43e864662f40d
Sandy <sandycris.luar@gmail.com>
2022-08-17 17:20:14 -0300

    primeira versao do projeto Java

---
+++ Sistema Academia/SistemaAcademia.java
@@ -0,0 +1,30 @@
+package com.academia.sistema;
+
+import java.util.Scanner;
+
+import com.academia.sistema.model.DadosAlune;
+
+public class SistemaAcademia {
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+
+		int op = 0;
+
+		DadosAlune c1primeiroAcesso = new DadosAlune();
+		DadosAlune c2login = new DadosAlune();
+		System.out.println("\t\t **********************");
+		System.out.println("\t\t **********************");
+		System.out.println("\t\t **** ------------ ****");
+		System.out.println("\t\t **** BEM - VINDE! ****");
+		System.out.println("\t\t **** ------------ ****");
+		System.out.println("\t\t **********************");
+		System.out.println("\t\t **********************");
+		System.out.println("\tDigite (1) para se cadastrar em nosso programa");
+
+		op = leia.nextInt();
+		if (op == 1) {
+			c1primeiroAcesso.primeiroAcesso();
+		}
+
+	}
+}
